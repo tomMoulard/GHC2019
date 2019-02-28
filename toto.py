4fed98fe12466d18e2f45f27cbe9748d83d7a828
Thibault Marchal <thibault.marchal@epita.fr>
2019-02-28 21:25:48 +0100

    push

--- toto.py
+++ toto.py
@@ -6,11 +6,9 @@ from random import randrange
 debug = False
 
 
-@trace
 def sim(a, b):
     return 1 - diff(a, b)
 
-@trace
 def diff(a, b):
     s = 0
     for e in a:
@@ -19,14 +17,12 @@ def diff(a, b):
     s /= max((len(a), len(b)))
     return 1 - s
 
-@trace
 def NOPEget_closest(img, l):
     if not l:
         return img
     return l[-1]
     return l[randrange(len(l))]
 
-@trace
 def get_closest(img, l):
     if not l:
         return img
@@ -34,34 +30,30 @@ def get_closest(img, l):
     di = diff(img.tags, l[0].tags)
     for i in l[1:]:
         d = diff(img.tags, i.tags)
-        if d < di:
+        if d < di and not i.visited:
             di = d
             res = i
     return res
 
-@trace
 def get_sorted(l):
     res = []
     e = l[0]
     for i in l:
-        if not i.visited:
-            i.visited= True
-            res.append(e)
-            l.remove(e)
-            e = get_closest(res[-1], l)
+        res.append(e)
+        e.visited = True
+        l.remove(e)
+        e = get_closest(res[-1], l)
     res.append(e)
     l.remove(e)
     #res.append(get_closest(res[-1], l))
     return res
 
-@trace
 def merge(a, b):
     for e in a:
         if not e in b:
             b.append(e)
     return b
 
-@trace
 def get_merged(l):
     res = []
     v = []
@@ -79,15 +71,26 @@ def get_merged(l):
     return res
 
 l = input_file(argv[1])
+l = get_merged(l)
+print(len(l))
+
+res = []
+
+scope = 201
+
+for i in range(0, len(l), scope):
+    print("\r", str(100 * i / len(l))[:2], "%", i,  end='                                 ')
+    if i + scope > len(l):
+        d = l[i:]
+        if d:
+            res += get_sorted(d)
+    else:
+        res += get_sorted(l[i:i+scope])
+print()
 
 s = argv[1].split("/")[-1]
 with open("res_" + s + ".txt", "w+") as f:
-    l = get_merged(l)
-    tmp = len(l) // 1000
-    for x  in range(1000):
-        ll= get_sorted(l[tmp * x:tmp * (x + 1)])
-        print(len(ll), file=f)
-        for i in ll:
-            print(i.h, i.tags)
-            print(i.id, file=f)
+    print(len(res), file=f)
+    for i in res:
+        print(i.id, file=f)
 
