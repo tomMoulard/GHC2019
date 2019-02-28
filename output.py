a96621eea81410d54c20a979a86f2464a87b8867
Lionel Vincke <lionel.vincke@epita.fr>
2019-02-28 19:44:13 +0100

    output up

--- output.py
+++ output.py
@@ -1,4 +1,9 @@
-fil = open("data.txt", "a")
+from tom import *
+from input import *
 count=0
-def output_file(res):
-    fil.write(" ".join(str(x) for x in res))
+def output_file(name, res):
+    name.write(" ".join(str(x) for x in res)+"\n")
+
+def bite(file_name, file2):
+    f=open(file_name,"r+")
+    m(input_file(file2), f)
