e78a69c5bb868e57a999472c09cf2810b6a7079f
Lionel Vincke <lionel.vincke@epita.fr>
2019-02-28 19:50:18 +0100

    test

--- output.py
+++ output.py
@@ -3,8 +3,16 @@ from input import *
 count=0
 def output_file(name, res):
     name.write(" ".join(str(x) for x in res)+"\n")
+    count+=1
 
 def bite(file_name, file2):
     f=open(file_name,"r+")
     m(input_file(file2), f)
     print(file2)
+
+
+def nb_line(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(str(count) + '\n' + content)
