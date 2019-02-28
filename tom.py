908d596c5449c49b3a355b1ffd9fb932b3a670dc
Tom Moulard <tom.moulard@epita.fr>
2019-02-28 19:47:15 +0100

    merde

--- tom.py
+++ tom.py
@@ -1,7 +1,7 @@
 from input import *
 from output import *
 
-def m(l):
+def m(l, f):
     print(l)
     this = l[0]
     while len(l) > 0:
@@ -11,13 +11,33 @@ def m(l):
                 break
             for tag in this.tags:
                 if tag in x.tags:
-                    print("pushing", x.id, x.tags)
-                    output_file(x)
+                    output_file(f, [x])
                     l.remove(x)
                     this = x
                     b = True
                     break
 
+fil = open("a_data.txt", "r+")
+print("provided/a_example.txt")
 m(input_file("provided/a_example.txt"))
+fil.close()
 
+fil = open("b_data.txt", "r+")
+print("b_lovely_landscapes.txt")
+m(input_file("provided/b_lovely_landscapes.txt"))
+fil.close()
+
+fil = open("c_data.txt", "r+")
+print("c_memorable_moments.txt")
+m(input_file("provided/c_memorable_moments.txt"))
+fil.close()
+
+fil = open("d_data.txt", "r+")
+print("d_pet_pictures.txt")
+m(input_file("provided/d_pet_pictures.txt"))
+fil.close()
+
+fil = open("e_data.txt", "r+")
+print("e_shiny_selfies.txt")
+m(input_file("provided/e_shiny_selfies.txt"))
 fil.close()
