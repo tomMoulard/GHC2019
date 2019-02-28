f54145b1143a79e056183ab8321c0f3976d668ca
Thibault Marchal <thibault.marchal@epita.fr>
2019-02-28 18:13:17 +0100

    debugger added

---
+++ debug.py
@@ -0,0 +1,27 @@
+global indent, debug
+indent = 0
+debug = True
+
+
+def trace(f):
+    def wrap(*args, **kwargs):
+        global indent, debug
+        if not debug:
+            return f(*args, **kwargs)
+        print(indent * " ", f.__name__, "entered with", args, kwargs)
+        indent += 4
+        r = f(*args, **kwargs)
+        indent -= 4
+        print(indent * " ", f.__name__, "out")
+        return r
+    return wrap
+
+@trace
+def f(i):
+    if i:
+        return f(i - 1)
+
+def g(i):
+    print(f(i))
+
+g(4)
